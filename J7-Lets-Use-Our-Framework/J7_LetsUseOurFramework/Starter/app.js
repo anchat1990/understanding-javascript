570fd7c6db458ca4ddb9bd6c3faef48e3489ad06
Anwesha Chatterjee <anchat1990@gmail.com>
2017-03-31 13:26:34 +0530

    commenting :P

--- J7-Lets-Use-Our-Framework/J7_LetsUseOurFramework/Starter/app.js
+++ J7-Lets-Use-Our-Framework/J7_LetsUseOurFramework/Starter/app.js
@@ -3,6 +3,9 @@
 //g.HTMLGreeting("#greeting")
 $("#login").click(function() {
     var loginGrtr = G$("Madhumita", "Chatterjee");
+    // when login button clicked, hide the login div
     $("#logindiv").hide();
+    // demonstrating function chaining. set language to the selected language, 
+    // then update using HTMLGreeting method, then log.
     loginGrtr.setLang($("#lang").val()).HTMLGreeting("#greeting", true).log();
 })
